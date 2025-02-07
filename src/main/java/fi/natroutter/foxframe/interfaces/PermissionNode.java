ddc53d14733a75e7bb476c0a300a752d9384cdbe
NATroutter <natroutter@gmail.com>
2025-02-07 21:23:06 +0200

    Fixed PermissionNode type

--- src/main/java/fi/natroutter/foxframe/interfaces/PermissionNode.java
+++ src/main/java/fi/natroutter/foxframe/interfaces/PermissionNode.java
@@ -2,15 +2,6 @@ package fi.natroutter.foxframe.interfaces;
 
 import lombok.Getter;
 
-public enum PermissionNode {
-
-
-
-    ;
-    @Getter
-    private String node;
-    PermissionNode(String node) {
-        this.node = node;
-    }
-
+public interface PermissionNode {
+    String getNode();
 }
